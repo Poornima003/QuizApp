b550ff4bd74e04ef59b03cea8b28924046a20cfb
Poornima <poornimamurali2803@gmail.com>
2023-11-06 16:19:47 +0530

    Final commit

---
+++ src/Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "A copy of the 'real' DOM that is kept in memory is called what?",
+        options:[ "Virtual DOM  ", "DOM", "Shadow DOM", "React DOM"],
+        answer: 1
+    },
+    {
+        question: "Which operator can be used to conditionally render a React component?",
+        options:[" || ", "::", "&&", "??"],
+        answer: 3
+    },
+    {
+        question: "What tool does React use to compile JSX?",
+        options: ["ReactDOM", "Babel", "React Router", "JSX Compiler"],
+        answer: 2
+    },
+    {
+        question: "What is a common use case for ref?",
+        options: ["To refer to a function ", "To bind the function", "To refer to another JavaScript file", "To directly access a DOM node "],
+        answer:4
+    },
+    {
+        question: "React separates the user interface into components. How are components combinded to create a user interface?",
+        options: ["By nesting components", "With webpack", "By putting them in a folder structure", "With code splitting"],
+        answer:1
+    },
+];
